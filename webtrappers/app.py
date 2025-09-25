0542a4808c7347eb7737cb4a601723c091195796
strtsnm <64964414+STRTSNM@users.noreply.github.com>
2025-09-25 14:57:22 +0530

    added url analysis support

--- webtrappers/app.py
+++ webtrappers/app.py
@@ -3,12 +3,15 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+#curl -X POST http://127.0.0.1:5000/check_url      -H "Content-Type: application/json"      -d '{"url": "https://google.com"}'
+
 @app.route('/check_url', methods=['POST'])
 def check_url():
     data = request.get_json(silent=True) or {}
     url = data.get("url", "")
 
-    score = 42  
+    score = model.inferURL(url)
+    print("URL : ", score)
 
     return jsonify({"score": score})
 
@@ -18,7 +21,7 @@ def check_text():
     data = request.get_json(silent=True) or {}
     text = data.get("text", "")
 
-    score = model.infer(text)
+    score = model.inferText(text)
 
     return jsonify({"score": score})
 
--- webtrappers/model.py
+++ webtrappers/model.py
@@ -1,15 +1,57 @@
 import time
 import multiprocessing
-from transformers import pipeline
+import torch
+from transformers import BertTokenizerFast, BertForSequenceClassification, pipeline
+from huggingface_hub import snapshot_download
 
-pipe = pipeline("text-classification", model="ealvaradob/bert-finetuned-phishing")
+def get_local_model_path(model_id: str) -> str:
+    """
+    Resolve the local snapshot path for a Hugging Face model ID.
+    If it's already cached, it won't re-download.
+    """
+    return snapshot_download(model_id, local_files_only=True)
 
+model_path1 = get_local_model_path("ealvaradob/bert-finetuned-phishing")
 
+pipe1 = pipeline(
+    "text-classification",
+    model=model_path1
+)
 
-def infer(p):
+def inferText(p):
     print(f"Process {multiprocessing.current_process().name} is analyzing a chunk...")
     t1 = time.time()
-    res = pipe(p)
+    res = pipe1(p)
     t2 = time.time()
     print(f"Chunk processed in {t2 - t1:.4f}s with result: {res}")
     return res[0]
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f"Device set to use: {device}")
+
+
+model_path2 = get_local_model_path("CrabInHoney/urlbert-tiny-v3-phishing-classifier")
+
+tokenizer = BertTokenizerFast.from_pretrained(model_path2, local_files_only=True)
+model = BertForSequenceClassification.from_pretrained(model_path2, local_files_only=True)
+model.to(device)
+
+classifier = pipeline(
+    "text-classification",
+    model=model,
+    tokenizer=tokenizer,
+    device=0 if torch.cuda.is_available() else -1,
+    return_all_scores=True
+)
+
+def inferURL(url):
+    label_mapping = {"LABEL_0": "good", "LABEL_1": "phish"}
+
+    results = classifier(url)[0]
+    print(results[1])
+    return results[1]['score']
+
+
+#inferURL("https://google.com")
+
